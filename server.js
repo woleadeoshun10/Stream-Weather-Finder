38477da4493ffefe37b6f119c2088441d5ff90dc
Adewole Adeoshun <146618781+woleadeoshun10@users.noreply.github.com>
2026-04-24 23:39:42 -0400

    Rename file system module variable from fs to f

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 Project
 */
 
-const fs  = require("fs"); // file system module
+const f  = require("fs"); // file system module
 const http = require("http"); // http module - to create web server
 const https = require("https"); // https module - to make requets to external APIs
 
